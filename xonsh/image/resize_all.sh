a2a2ebd56787877368b06f7fc3b2c185ae102f71
vvasuki <vishvas.vasuki@gmail.com>
2022-04-27 17:01:17 +0530

    CU

--- xonsh/image/resize_all.sh
+++ xonsh/image/resize_all.sh
@@ -2,4 +2,4 @@
 
 for fpath in pg`*.jpg`:
   # convert @(fpath) -resize 50% -quality 30% @(str(fpath).replace(".jpg", ""))-50p.jpg
-  convert @(fpath) -resize 50% -quality 30% @(fpath)
+  convert @(fpath) -quality 30% @(fpath)
